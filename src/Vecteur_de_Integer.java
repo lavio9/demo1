a68e66e6345f413a5b23734a176d97bb47116a69
Arthur <arthur.laumay@icloud.com>
2022-11-27 11:34:09 +0100

    test

---
+++ src/Vecteur_de_Integer.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Vecteur_de_Integer {
+
+    public static int saisieEntPos() {
+
+    }
+
+    ArrayList<String> lol = new ArrayList<>()   ;
+    public static ArrayList<Integer> lesDiviseurs(int unEnt) {
+
+    }
+
+
+}
